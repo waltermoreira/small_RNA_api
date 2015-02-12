2bc2a991bc12b1defbad7aa428ec997b487a6f99
JasonRafeMiller <jmiller@jcvi.org>
2015-02-12 04:10:47 -0500

    Reformat the JSON with the aim of making it easier to parse. Omit the query metadata like species. For each small RNA, generate one JSON object. Within each small RNA, filter out redundant fields like length. Within each small RNA, separate the header data like sequence from the table of abundances.

--- main.py
+++ main.py
@@ -1,8 +1,64 @@
 # file: main.py
 
+import requests
 import json
 
-def search(args):
-    print json.dumps({'obj': 1, 'args': args})
-    print "---"
-    print json.dumps({'obj': 2, 'args': args})
+def list(arg):
+    pass
+
+def search(arg):
+
+    # The requests object contains HTTP headers plus payload.
+    # print r.headers
+    # The remote service sets cookies. For now, we do not pass the cookie.
+    # print headers['set-cookie']
+
+    # The output may be ordered differently than the input.
+    # In particular, the "sequence" tuple may be buried in the list.
+
+    # For now, hard code an example URL.
+    url = 'http://mpss.udel.edu/web/php/pages/abundances.php?SITE=at_sRNA&chrnum=1&beg=9000&end=9400&format=json'
+    rqst = requests.get(url)
+
+    if rqst.ok:
+        content_type = rqst.headers['content-type']
+        if "application/json" in content_type:
+            payload = rqst.json()
+            # The payload includes this metadata.
+            # payload['SITE'] = "at_sRNA"
+            # payload['annotation'] = "TAIR10"
+            # payload ['species'] = "Arabidopsis thaliana"
+            # For now, we will filter out the metadata.
+            lib_abundances = payload['lib_abundances']
+            for one_seq in lib_abundances:                
+                # Each one_seq includes the key tuple "sequence": "AAAGGGAAAGAACCC",
+                # and a few descriptive tuples (hits, position, strand, length).
+                # Some are inferrable from the data: hits, length.
+                # Separate out the others.
+                seq_sequence = one_seq['sequence']
+                seq_position = one_seq['position']
+                seq_strand   = one_seq['strand']
+                del one_seq['sequence']
+                del one_seq['position']
+                del one_seq['strand']
+                del one_seq['hits']
+                del one_seq['length']
+                # What is left is many tuples of <line>:<abundance> like "Col0":0.
+                one_rec = {
+                        'sequence':seq_sequence,
+                        'position':seq_position,
+                        'strand':seq_strand,
+                        'abundance_table':one_seq}
+                decoded = json.dumps(one_rec)
+                print decoded
+                print '---'
+            
+        
+        else:
+            return
+    
+
+        
+    
+
+
